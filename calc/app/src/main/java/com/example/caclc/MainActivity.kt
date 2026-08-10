a0d9c98a98b672832da93f24ceaf2ff8d9f4c10f
BharathiKoruprolu25 <bharathikoruprolu25@gmail.com>
2026-08-10 09:43:17 +0530

    Resolve Lint errors and missing class exceptions - Created Main2Activity and LoginResultActivity - Updated AndroidManifest.xml with correct activity names - Fixed tools:context in layout files

--- calc/app/src/main/java/com/example/caclc/MainActivity.kt
+++ calc/app/src/main/java/com/example/caclc/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.caclc
 
+import android.content.IntentFilter
 import android.os.Bundle
 import android.util.Log
 import android.widget.Button
@@ -7,10 +8,13 @@ import android.widget.EditText
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.os.unregisterForAllProfilingResults
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
